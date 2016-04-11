0013cadfe7f39c7612b04825c5d314627ea4afd4
845860421 <millen.c.wong@gmail.com>
2016-04-12 10:05:00 +1200

    arrays, loop, interactive: button, onclick

--- js/intro.js
+++ js/intro.js
@@ -10,14 +10,14 @@ comment.
 */
 
 //Variable that holds my name
-var myName = "Millen C. Wong";
+var myName = "Millen C. Wong"; //This is an input
 
 //How many characters are in my name
 console.log(myName);
 console.log(myName.length);  /*numbers of characters in myname */
 
 //Variable that holds my age
-var myAge = 22;
+var myAge = 22;          //This is another input
 console.log(myAge);
 
 //If the user is older than 21
@@ -48,5 +48,35 @@ else {
 if (myName.length == 0) {
 	console.log("Your name is required.");
 }
-else if (myName.length > 10)
-	console.log("Name should be at most 10 characters. You have entered " + myName.length + " characters");
+else if (myName.length > 10) {
+	console.log("Name should be at most 10 characters. Yours has " + myName.length + " characters.");
+}
+
+
+// //An array (list, collection, etc) of people
+// console.log(" ");
+// console.log("Arrays");
+var people = ["Millen", "Dani", "Zach", "Mai2"];
+console.log( people );
+console.log("There are " + people.length + " people.");
+console.log( people [0]);   //Should display "Millen"
+console.log( people [1]);   //Should display "Dani"
+
+//Lconsole.logop over all the people in the array
+//and display their name in the console.
+console.log(" ");
+console.log("Lessons on loop");
+console.log(" ");
+
+for (var i = 0; i < people.length; i++)   //init i to 0; loop while i < 3 (because i==0,1,2,3); i= i + 1
+{
+	console.log(people[i]);
+}
+
+//Tell JS to find the button with id="hello"
+var helloButton = document.querySelector("#hello"); //searches for the location of the id "hello"
+
+//Listens for clicks on the hello button
+helloButton.onclick = function() {
+	alert('Hi to you');
+}
